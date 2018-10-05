962bb9cd18a16a3e3c6475d7a008c76df769ca5d
Craig S. Cottingham <craig@cottingham.net>
2018-10-05 17:36:25 -0500

    Set preferred env for espec to :test

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Olagarro.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      preferred_cli_env: [espec: :test],
+      deps: deps(),
     ]
   end
 
