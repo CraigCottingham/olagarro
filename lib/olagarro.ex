6300304967af98637b2921114fea2b7acce98f21
Craig S. Cottingham <craig@cottingham.net>
2018-10-05 15:50:17 -0500

    Generate new project

---
+++ lib/olagarro.ex
@@ -0,0 +1,18 @@
+defmodule Olagarro do
+  @moduledoc """
+  Documentation for Olagarro.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Olagarro.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/olagarro_test.exs
@@ -0,0 +1,8 @@
+defmodule OlagarroTest do
+  use ExUnit.Case
+  doctest Olagarro
+
+  test "greets the world" do
+    assert Olagarro.hello() == :world
+  end
+end
